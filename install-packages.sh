7e20352a53865bdc4e329d08bf4e1e5185c1e680
cyberme0w <iuri_archer@hotmail.com>
2022-08-31 19:13:50 +0200

    Final commit before fresh start

--- install-packages.sh
+++ install-packages.sh
@@ -4,37 +4,36 @@ if [[ $UID != 0 ]]; then
     exit
 fi
 
-echo -n "These packages are supposed to be installed on top of a Debian minimal installation. Proceed? (y/N) "
-read $yn
-if [[ $yn =~ y|Y ]]; then
-    pkgs=""
-    pkgs=$pkgs"cmus "
-    pkgs=$pkgs"gimp "
-    pkgs=$pkgs"git "
-    pkgs=$pkgs"neofetch "
-    pkgs=$pkgs"neovim "
-    pkgs=$pkgs"qalc "
-    pkgs=$pkgs"qemu "
-    pkgs=$pkgs"ranger "
-    pkgs=$pkgs"ruby "
-    pkgs=$pkgs"setxkbmap "
-    pkgs=$pkgs"scrot "
-    pkgs=$pkgs"subversion "
-    pkgs=$pkgs"tar "
-    pkgs=$pkgs"testdisk "
-    pkgs=$pkgs"texlive-base "
-    pkgs=$pkgs"tree "
-    pkgs=$pkgs"thunderbird "
-    pkgs=$pkgs"which "
-    pkgs=$pkgs"wireshark "
-    pkgs=$pkgs"xrandr "
-    pkgs=$pkgs"zathura "
+pkgs=(
+    cmus
+    gimp
+    git
+    neofetch
+    neovim 
+    qalc
+    qemu
+    ranger
+    ruby
+    setxkbmap
+    scrot
+    subversion
+    tar
+    testdisk
+    texlive-base
+    tree
+    thunderbird
+    which
+    wireshark
+    zathura
+)
     
-    echo -n "${#pkgs[@]} Packages will be installed. Proceed? (y/N) "
-    read input
+echo -n "${#pkgs[@]} Packages will be installed. Proceed? (y/N) "
+read input
 
-    if [[ $input =~ ^Y$|^y$ ]]; then
+if [[ $input =~ ^Y$|^y$ ]]; then
     echo "Installing..."
-    apt install $pkgs
+    for pkg in $pkgs; do
+	apt install $pkg -y
+    done
     echo "Done!"
 fi

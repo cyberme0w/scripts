e1e340445b2a85392550b7ec643b3e333fa005c6
cyberme0w <iuri_archer@hotmail.com>
2022-09-11 09:51:29 +0200

    Updates

--- setup_wp.sh
+++ setup_wp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 NORDBG="#2e3440"
-WALLPAPERPATH="/home/cyberme0w/Pictures/wallpapers/nord/*"
+WALLPAPERPATH="/home/cyberme0w/Pictures/wallpapers/nord/nord-001-fish.png"
 #feh --bg-max --no-fehbg $WALLPAPERPATH -B $NORDBG
 feh --bg-center --no-fehbg $WALLPAPERPATH -B $NORDBG
